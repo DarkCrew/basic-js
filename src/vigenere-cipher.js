1cf1ffd70784ec8f6d59806e4c14c0a4279c8e0d
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-05-01 14:27:35 +0300

    feat 21 tasks ready

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -25,13 +25,82 @@ class VigenereCipheringMachine {
     this.arr_en = 'abcdefghijklmnopqrstuvwxyz';
   }
 
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(text = '', key = '') {
+    let newStr = '';
+    let j = 0;
+    if(key === '' || text === '') throw new Error("Incorrect arguments!");
+
+    if(key.length != text.length){
+      if(key.length > text.length){
+        text = text.slice(0, key.length);
+      }else{
+        key = key.repeat(Math.ceil(text.length / key.length));
+        key = key.slice(0, text.length);
+      }
+    }
+
+    for(let i = 0; i < text.length; i++){
+      if(this.arr_en.indexOf(text[i].toLowerCase()) === -1){
+        newStr += text[i];
+      }else{
+        let indexOfText = this.arr_en.indexOf(text[i].toLowerCase());
+        console.log(`indexOfText: ${indexOfText}`);
+        let indexOfKey = this.arr_en.indexOf(key[j].toLowerCase());
+        console.log(`indexOfKey: ${indexOfKey}`);
+        let indexSum = 0;
+        indexSum = (indexOfText + indexOfKey) >= this.arr_en.length ? (indexOfText + indexOfKey) % 26 : (indexOfText + indexOfKey);
+        console.log(`indexSum: ${indexSum}`);
+        newStr += this.arr_en[indexSum];
+        j++;
+      }
+    }
+
+    if(this.machine){
+      return newStr.toUpperCase();
+    }else{
+      return [...(newStr.toUpperCase())].reverse().join('');
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(text = '', key = '') {
+    let newStr = '';
+    let j = 0;
+    if(key === '' || text === '') throw new Error("Incorrect arguments!");
+
+    if(key.length != text.length){
+      if(key.length > text.length){
+        text = text.slice(0, key.length);
+      }else{
+        key = key.repeat(Math.ceil(text.length / key.length));
+        key = key.slice(0, text.length);
+      }
+    }
+
+    for(let i = 0; i < text.length; i++){
+      if(this.arr_en.indexOf(text[i].toLowerCase()) === -1){
+        newStr += text[i];
+      }else{
+        let indexOfText = this.arr_en.indexOf(text[i].toLowerCase());
+        console.log(`indexOfText: ${indexOfText}`);
+        let indexOfKey = this.arr_en.indexOf(key[j].toLowerCase());
+        console.log(`indexOfKey: ${indexOfKey}`);
+        let indexSum = 0;
+        if(indexOfText - indexOfKey < 0){
+          indexSum = ((this.arr_en.length) - Math.abs(indexOfText - indexOfKey));
+        }else{
+          indexSum = indexOfText - indexOfKey;
+        }
+        console.log(`indexSum: ${indexSum}`);
+        
+        newStr += this.arr_en[indexSum];
+        j++;
+      }
+    }
+
+    if(this.machine){
+      return newStr.toUpperCase();
+    }else{
+      return [...(newStr.toUpperCase())].reverse().join('');
+    }
   }
 }
 
